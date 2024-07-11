ee36a7362061e7e308c9924b83301e6f9a570998
kenedy4 <kennedymusyoki8@gmail.com>
2024-07-11 14:04:25 +0300

    Edited the last errors

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-// function fetchBooks() {
-//   // To pass the tests, don't forget to return your fetch!
-//   return fetch("https://example.com/api/books")
-//     .then((response) => response.json()) // Parse the JSON from the response
-//     .then((data) => {
-//       renderBooks(data); // Pass the data to the renderBooks function
-//     })
-//     .catch((error) => {
-//       console.error("Error fetching books:", error); // Handle any errors
-//     });
-// }
-
-// function renderBooks(books) {
-//   const main = document.querySelector("main");
-//   books.forEach((book) => {
-//     const h2 = document.createElement("h2");
-//     h2.innerHTML = book.name;
-//     main.appendChild(h2);
-//   });
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   fetchBooks();
-// });
 function fetchBooks() {
-  // Fetch data from the API endpoint
+  // To pass the tests, don't forget to return your fetch!
   return fetch("https://anapioficeandfire.com/api/books")
-    .then((response) => response.json()) // Parse the JSON from the response
+    .then((response) => response.json())
     .then((data) => {
-      renderBooks(data); // Pass the data to the renderBooks function
+      renderBooks(data);
     })
     .catch((error) => {
-      console.error("Error fetching books:", error); // Handle any errors
+      console.error("Error fetching books:", error);
     });
 }
 
